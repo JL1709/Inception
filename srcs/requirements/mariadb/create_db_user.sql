21d82d615cbdcbd1ca4b72ca2e856a9c0dbe2a9b
jludt <julian.lu@web.de>
2022-04-12 17:03:46 +0200

    still commenting

--- srcs/requirements/mariadb/create_db_user.sql
+++ srcs/requirements/mariadb/create_db_user.sql
@@ -1,4 +1,4 @@
-
+-- https://www.digitalocean.com/community/tutorials/how-to-install-mariadb-on-debian-10
 CREATE DATABASE wordpress;
 CREATE USER 'jludt'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'jludt'@'%';
